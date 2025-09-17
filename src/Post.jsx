ea4b68c197ebca16de529dc1a439130c56917e53
Joinal Abden <joinalzx@gmail.com>
2025-09-17 14:06:10 +0600

    react core concept done

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,10 +1,11 @@
 export default function Post({ post }) {
   console.log(post);
-  const { userId, title } = post;
+  const { userId, title, body } = post;
   return (
     <div className="card">
       <h4>Single Post: {userId}</h4>
       <h4>Title: {title}</h4>
+      <p>Body: {body}</p>
     </div>
   );
 }
